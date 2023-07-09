e05a938a14a6c455336a5fcd6ac9c9c15f6d68e5
Alexander <bmgula55@mail.ru>
2023-07-09 16:34:17 +0300

    ref func test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -11,3 +11,9 @@ class TestArrs(unittest.TestCase):
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+
+        self.assertIsInstance(arrs.my_slice([], 1), list)
+        self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([], 1), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -4), [1, 2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -1), [3])
